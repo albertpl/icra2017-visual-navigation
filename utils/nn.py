3888c5ae3f2f09c5c84643ca242eeb03ce7e3457
Albert Liu <peng_liu@apple.com>
2017-06-03 15:25:37 -0700

    adding missing files

---
+++ utils/nn.py
@@ -0,0 +1,89 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib import layers
+
+
+def leaky_relu(x, alpha=0.2):
+    """Compute the leaky ReLU activation function.
+
+    Inputs:
+    - x: TensorFlow Tensor with arbitrary shape
+    - alpha: leak parameter for leaky ReLU
+
+    Returns:
+    TensorFlow Tensor with the same shape as x
+    """
+    return tf.maximum(x, alpha * x)
+
+
+def siamese(x1, x2, out_dim, name):
+    # flatten input
+    x1_size = np.prod(x1.get_shape().as_list()[1:])
+    x2_size = np.prod(x2.get_shape().as_list()[1:])
+    assert x1_size == x2_size
+    with tf.variable_scope(name):
+        x1 = tf.reshape(x1, [-1, x1_size], name='x1')
+        x2 = tf.reshape(x2, [-1, x1_size], name='x2')
+        w = tf.get_variable('weight', (x1_size, out_dim), initializer=layers.variance_scaling_initializer())
+        b = tf.get_variable('bias', (out_dim,), initializer=tf.constant_initializer())
+        x1 = tf.matmul(x1, w) + b
+        x1 = leaky_relu(x1)
+        x2 = tf.matmul(x2, w) + b
+        x2 = leaky_relu(x2)
+        x = tf.concat(values=[x1, x2], axis=1)
+    return x
+
+
+def add_summary(writer, value_dict, global_step=0):
+    if writer is None or len(value_dict) == 0:
+        return
+    value = [tf.Summary.Value(tag=k, simple_value=v) for k, v in value_dict.items()]
+    summary = tf.Summary(value=value)
+    writer.add_summary(summary, global_step=global_step)
+    return
+
+
+def sigmoid(x):
+    """
+    A numerically stable version of the logistic sigmoid function.
+    """
+    pos_mask = (x >= 0)
+    neg_mask = (x < 0)
+    z = np.zeros_like(x)
+    z[pos_mask] = np.exp(-x[pos_mask])
+    z[neg_mask] = np.exp(x[neg_mask])
+    top = np.ones_like(x)
+    top[neg_mask] = z[neg_mask]
+    return top / (1 + z)
+
+
+def var_shape(x):
+    out = [k.value for k in x.get_shape()]
+    assert all(isinstance(a, int) for a in out), "shape is not fully known"
+    return out
+
+
+def numel(x):
+    return np.prod(var_shape(x))
+
+
+def flat_grad(loss, var_list):
+    grads = tf.gradients(loss, var_list)
+    return tf.concat([tf.reshape(grad, [numel(v)]) for (v, grad) in zip(var_list, grads)], axis=0)
+
+
+def add_get_from_flat(var_list):
+    return tf.concat([tf.reshape(v, [numel(v)]) for v in var_list], 0)
+
+
+def add_assign_from_flat(var_list, var_v):
+    size = [numel(v) for v in var_list]
+    assert sum(size) == var_v.get_shape().as_list()[0]
+    start = 0
+    assign_ops = []
+    for s, v in zip(size, var_list):
+        shape = var_shape(v)
+        assign_ops.append(tf.assign(v, tf.reshape(var_v[start:start + s], shape)))
+        start += s
+    return var_v, tf.group(*assign_ops)
+
---
+++ utils/rl.py
@@ -0,0 +1,102 @@
+import numpy as np
+import logging
+import random
+from gail.trajectory import RaggedArray
+
+def choose_action_label_smooth(config, action, epsilon):
+    """ epsilon-greedy: P(k) =  (1-epsilon) * P_e +  e * 1/N """
+    pi_values = [epsilon/float(config.action_size)] * config.action_size
+    pi_values[action] += 1-epsilon
+    return pi_values
+
+
+def choose_action_greedy(pi_values):
+    # greedy algorithm since this is supervised learning
+    return np.argmax(pi_values, axis=0)
+
+
+def choose_action(pi_values):
+    values = []
+    s = 0.0
+    for rate in pi_values:
+        s += rate
+        values.append(s)
+    r = random.random() * s
+    for i in range(len(values)):
+        if values[i] >= r:
+            return i
+    # fail safe
+    return len(values) - 1
+
+
+def get_key(scopes):
+    return '/'.join(scopes)
+
+
+def discount(r_N_T_D, gamma):
+    '''
+    Computes Q values from rewards.
+    q_N_T_D[i,t,:] == r_N_T_D[i,t,:] + gamma*r_N_T_D[i,t+1,:] + gamma^2*r_N_T_D[i,t+2,:] + ...
+    '''
+    assert r_N_T_D.ndim == 2 or r_N_T_D.ndim == 3
+    input_ndim = r_N_T_D.ndim
+    if r_N_T_D.ndim == 2: r_N_T_D = r_N_T_D[...,None]
+
+    discfactors_T = np.power(gamma, np.arange(r_N_T_D.shape[1]))
+    discounted_N_T_D = r_N_T_D * discfactors_T[None,:,None]
+    q_N_T_D = np.cumsum(discounted_N_T_D[:,::-1,:], axis=1)[:,::-1,:] # this is equal to gamma**t * (r_N_T_D[i,t,:] + gamma*r_N_T_D[i,t+1,:] + ...)
+    q_N_T_D /= discfactors_T[None,:,None]
+
+    # Sanity check: Q values at last timestep should equal original rewards
+    assert np.allclose(q_N_T_D[:,-1,:], r_N_T_D[:,-1,:])
+
+    if input_ndim == 2:
+        assert q_N_T_D.shape[-1] == 1
+        return q_N_T_D[:,:,0]
+    return q_N_T_D
+
+
+def compute_qvals(r, gamma):
+    assert isinstance(r, RaggedArray)
+    trajlengths = r.lengths
+    # Zero-fill the rewards on the right, then compute Q values
+    rewards_B_T = r.padded(fill=0.)
+    qvals_zfilled_B_T = discount(rewards_B_T, gamma)
+    assert qvals_zfilled_B_T.shape == (len(trajlengths), trajlengths.max())
+    return RaggedArray([qvals_zfilled_B_T[i,:l] for i, l in enumerate(trajlengths)]), rewards_B_T
+
+
+def linesearch(f, x, fullstep, expected_improve_rate):
+    accept_ratio = .1
+    max_backtracks = 10
+    fval = f(x)
+    for (_n_backtracks, stepfrac) in enumerate(.5**np.arange(max_backtracks)):
+        xnew = x + stepfrac * fullstep
+        newfval = f(xnew)
+        actual_improve = fval - newfval
+        expected_improve = expected_improve_rate * stepfrac
+        ratio = actual_improve / expected_improve
+        logging.debug("%(expected_improve)f %(actual_improve)f" % locals())
+        if ratio > accept_ratio and actual_improve > 0:
+            return xnew
+    return x
+
+
+def conjugate_gradient(f_Ax, b, cg_iters=10, residual_tol=1e-10):
+    p = b.copy()
+    r = b.copy()
+    x = np.zeros_like(b)
+    rdotr = r.dot(r)
+    for i in range(cg_iters):
+        z = f_Ax(p)
+        v = rdotr / p.dot(z)
+        x += v * p
+        r -= v * z
+        newrdotr = r.dot(r)
+        mu = newrdotr / rdotr
+        p = r + mu * p
+        rdotr = newrdotr
+        if rdotr < residual_tol:
+            break
+    return x
+
